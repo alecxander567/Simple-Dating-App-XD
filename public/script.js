2eff089feb5f0ae72a77332c6069e59e3c14c88f
alecxander567 <alecxanderespaldon21@gmail.com>
2026-02-05 13:23:11 +0800

    separated javascript code from html file

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
 const fireflies = document.querySelectorAll(".firefly");
 
+function handleYes() {
+  window.location.href = "./chatroom.html";
+}
+
+function handleNo() {
+  const noBtn = event.target;
+  const randomX = Math.random() * (window.innerWidth - 200);
+  const randomY = Math.random() * (window.innerHeight - 100);
+  noBtn.style.position = "fixed";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+}
+
 function randomPosition() {
   return Math.random() * window.innerWidth;
 }
